91790ffaa098f16767b4cf6ae36f8405d963a9c9
Nima Sakha <nimasakha@gmail.com>
2023-05-25 20:45:35 +1000

    project init

---
+++ components/Nav.jsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react"
+import Button from "./Button"
+function Nav() {
+  return (
+    <div>
+      <Button variant='nav' onClick={() => null} />
+    </div>
+  )
+}
+
+export default Nav
